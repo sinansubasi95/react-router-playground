c3f8139f91cea753c673f9a745e86980f2970a13
sinansubasi <s.subasi@katre.com>
2021-01-28 00:14:49 +0300

    select filter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+// import { useLocation } from 'react-router';
+
+import List from './components/List'
 
 function App() {
+  // const { search } = useLocation();
+  // const searchParams = new URLSearchParams(search);
+  // const name = searchParams.get('name');
+  // const age = searchParams.get('age');
+
+  // console.log(search);
+
   return (
-    <div className="App">
-      test
+    <div style={{ maxWidth: "980px", margin: "70px auto 0"}}>
+      <List />
     </div>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,124 @@
+// eslint-disable-next-line no-restricted-globals
+
+import React from "react";
+import { withRouter } from 'react-router-dom'    
+import queryString from 'query-string';
+
+
+class List extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            employees: [
+                { 
+                    firstname: "Jill",
+                    lastname: "Smith",
+                    age: 50,
+                    company: "apple"
+                },
+                { 
+                    firstname: "Eve",
+                    lastname: "Jackson",
+                    age: 94,
+                    company: "apple"
+                },
+                { 
+                    firstname: "Mike",
+                    lastname: "Freakson",
+                    age: 32,
+                    company: "google"
+                },
+                { 
+                    firstname: "James",
+                    lastname: "Felixson",
+                    age: 18,
+                    company: "microsoft"
+                },
+            ],
+            companies: ["microsoft", "google", "apple", "samsung"],
+            selectedCompany: "",
+            minAge: "",
+            maxAge: ""
+        };
+    }
+    
+    componentDidMount() {
+        // const { search } = this.props.location;
+        // const searchParams = new URLSearchParams(search);
+        // for(var param of searchParams.entries()) {
+        //     console.log(param);
+        //     param.forEach((key) => {
+        //         console.log(key);
+        //         console.log("zz");
+        //         if(key === "company") {
+        //             this.setState({selectedCompany: param[0]});
+        //         }
+        //     });
+
+            // if(param[0] === "company") {
+            //     this.setState({selectedCompany: param[1]});
+            // }
+        // }
+    }
+
+    handleSelect = (e) => {
+        this.setState({selectedCompany: e.target.value});
+
+        let currentUrlParams = new URLSearchParams(window.location.search);
+        currentUrlParams.set('company', e.target.value);
+        this.props.history.push(window.location.pathname + "?" + currentUrlParams.toString());
+    }
+
+    
+    handleInput = (e) => {
+        if(e.target.name === "minAge") {
+            this.setState({minAge: e.target.value});
+        }
+
+        if(e.target.name === "maxAge") {
+            this.setState({maxAge: e.target.value});
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div>
+                    <select onChange={this.handleSelect} value={this.state.selectedCompany}>        
+                        {this.state.companies.map((company, i) => (     
+                            <option key={i}>{company}</option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <input onChange={this.handleInput} type="text" name="minAge" placeholder="min age" />
+                    <input onChange={this.handleInput} type="text" name="maxAge" placeholder="max age" />
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Age</th>
+                            <th>Company</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.employees.map((employee, i) => (   
+                            this.state.selectedCompany === employee.company ?                 
+                            <tr key={i}>
+                                <td>{employee.firstname}</td>
+                                <td>{employee.lastname}</td>
+                                <td>{employee.age}</td>
+                                <td>{employee.company}</td>
+                            </tr> : null
+                        ))}
+                    </tbody>
+                </table>
+            </>
+        );
+    }
+}
+
+export default withRouter(List);
